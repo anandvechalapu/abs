9a34c73ab10aaf502530a9ff81d63d644dfefc5a
anandvechalapu <anand.vechalapu@gmail.com>
2023-03-06 17:34:02 +0530

    initial commit

---
+++ History screen and fetching request information form java API and DB0.34780431181505433.java
@@ -0,0 +1,10 @@
+
+
+Solution:
+1. Create a Spring boot application with the necessary dependencies.
+2. Create a controller class with the necessary methods and endpoints required to display the requested code requests with their status and GitHub link.
+3. Create a service class with the necessary methods and logic to retrieve the requested code requests with their status and GitHub link from the API and database.
+4. Create a repository class to access the database and retrieve the requested code requests with their status and GitHub link.
+5. Implement the logic in the controller and service classes to display the requested code requests with their status and GitHub link on the History screen.
+6. Configure the screen to refresh based on the given configuration.
+7. Test the implementation and verify that it meets the acceptance criteria.
